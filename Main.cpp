b31a94bfc378f339cd99516f16d62be994ea2284
Paul Smith <s011075g@student.staffs.ac.uk>
2018-01-20 17:17:55 +0000

    Only a small change to the scope of the main

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,11 @@
 #include "WestWorld/WestWorldManager.h"
 int main()
 {
-	std::unique_ptr<WestWorldManager> westWorld = std::make_unique<WestWorldManager>();
-	const int result = westWorld->Start();
+	int result;
+	{
+		std::unique_ptr<WestWorldManager> westWorld = std::make_unique<WestWorldManager>();
+		result = westWorld->Start();
+	}
 	std::cin.get();
 	return result;
 }
